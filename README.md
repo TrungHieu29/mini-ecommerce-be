dc59bd71c556529b9fcfdbe3491d452fa31fefac
TrungHieu29 <ahieu2910@gmail.com>
2026-03-12 14:57:42 +0700

    init: setup spring boot project

---
+++ README.md
@@ -0,0 +1,13 @@
+# Mini Ecommerce Backend
+
+Take-home assignment - Backend
+
+## Tech Stack
+- Java 21
+- Spring Boot 3
+- Spring Data JPA
+- H2 Database
+- Lombok
+- Spring Boot DevTools
+- Spring Web
+- Validation
---
+++ src/main/java/com/example/mini_ecommerce_be/MiniEcommerceBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.mini_ecommerce_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniEcommerceBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniEcommerceBeApplication.class, args);
+	}
+
+}
